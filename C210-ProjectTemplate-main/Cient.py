2fd7ddb41334d0ca0396f4190df5c855ffd7016a
Vaishnavi1ab <94227105+Vaishnavi1ab@users.noreply.github.com>
2023-11-17 19:14:12 +0530

    Add files via upload

---
+++ C210-ProjectTemplate-main/Cient.py
@@ -0,0 +1,75 @@
+import socket
+from threading import Thread
+from tkinter import *
+from tkinter import ttk
+import ftplib
+import os
+import ntpath #This is used to extract filename from path
+
+from tkinter import filedialog
+from pathlib import Path
+
+
+from playsound import playsound
+import pygame
+from pygame import mixer
+
+
+PORT=8050
+IP_ADDRESS='127.0.0.1'
+SERVER=None
+BUFFER_SIZE=4096
+
+
+def musicWindow():
+    window=Tk()
+    window.title('Music Window')
+    window.geometry('300*300')
+    window.configure(bg='LightSkyBlue')
+
+    global selectlabel
+    global listbox
+
+    selectlabel = Label(window, text= "Select Song", bg='LightSkyBlue', font = ("Calibri",0))
+    selectlabel.place(x=2, y=1)
+
+    listbox = Listbox(window,height = 10,width = 39,activestyle = 'dotbox',bg='LightSkyBlue',borderwidth=2, font=('calibri',10) )
+    listbox.place(x=10, y=10)
+
+    scrollbar1 = Scrollbar(listbox)
+    scrollbar1.place(relheight = 1,relx = 1)
+    scrollbar1.config(command = listbox.yview)
+
+    PlayButton=Button(window,text='Play', width=10, bd=1, bg='SkyBlue', font=('Calibri',10))
+    PlayButton.place(x=30,y=200)
+   
+    Stop=Button(window,text="stop",bd=1, width=10,bd='SkyBlue', font = ("Calibri",10))
+    Stop.place(x=200,y=200)
+
+    Upload=Button(window,text="Upload",bd=1,width=10,bg='SkyBlue', font = ("Calibri",10), command = showClientsList)
+    Upload.place(x=30,y=250)
+
+    Download = Button(window, text= "Download",width=10,bd=1,bg='Skyblue', font = ("Calibri",10))
+    Download.place(x=200, y=250)
+
+    infoLabel = Label(window, text='',fg='blue',font = ("Calibri",10))
+    infoLabel.place(x=4,y=200)
+
+    
+    window.mainloop()
+
+
+
+
+
+def setup():
+    global SERVER
+    global PORT
+    global IP_ADDRESS
+
+    SERVER=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    SERVER.connect((IP_ADDRESS,PORT))
+
+    musicWindow()
+
+setup()    
